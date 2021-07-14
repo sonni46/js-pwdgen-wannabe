a87879c2359cbe83ade4ff8bf09f9c712f5b63fb
Marco <sonni46marcosenpai@gmail.com>
2021-07-14 13:25:47 +0200

    finito

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//  chiedere il nome cognome colore eta
+var Name = prompt("Dimmi il tuo nome?")
+
+var Surname = prompt("Dimmi il tuo cognome?")
+
+var Color = prompt("Dimmi il tuo colore preferito?")
+
+var Year = prompt("Quanti anni hai?")
+
+
+
+document.getElementById ("nome").innerHTML = Name + Surname + Color + Year
+
